20bf19bca3a80f9aa1ad2259795712f7546e9cd9
Tyler Conrad <conradt80@gmail.com>
2014-12-18 20:50:57 -0600

    Fix dp calculations.

--- seqer/uix/bpm_gauge.py
+++ seqer/uix/bpm_gauge.py
@@ -3,7 +3,8 @@ from kivy.uix.slider import Slider
 
 
 Builder.load_string('''
-#:import dp seqer.utils.map_dp
+#:import dp kivy.metrics.dp
+
 
 <-BPMGauge>:
     orientation: 'horizontal'
@@ -16,24 +17,24 @@ Builder.load_string('''
             rgb: 1.0, 1.0, 1.0
 
         BorderImage:
-            pos: dp(self.pos)
-            size: dp(self.size)
-            border: dp([16.0] * 4)
+            pos: self.pos
+            size: self.size
+            border: [dp(16.0)] * 4
             source: 'atlas://data/images/defaulttheme/button'
 
         Color:
             rgba: 0.2, 0.2, 0.8, 0.4
 
         Rectangle:
-            pos: dp(self.pos)
-            size: dp([self.value_normalized * self.width, self.height])
+            pos: self.pos
+            size: self.value_normalized * self.width, self.height
 
     Label:
-        pos: dp(root.pos)
-        size: dp(root.size)
-        # color: 0.8, 0.8, 0.8, 1.0
+        pos: root.pos
+        size: root.size
+        color: 0.8, 0.8, 0.8, 1.0
         text: str(root.value)
-        font_size: str(dp([0.8 * root.height])[0]) + 'dp'
+        font_size: 0.8 * root.height
 ''')
 
 
